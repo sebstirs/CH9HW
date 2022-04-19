17f946fe653450db91ff42c42803b0f7b1ef600a
sebstirs <98188443+sebstirs@users.noreply.github.com>
2022-04-19 18:25:23 -0400

    Add files via upload

---
+++ Exercise1.java
@@ -0,0 +1,7 @@
+public class Exercise1 {
+
+    public static void main(String[] args) {
+        int k, j, i;
+        for (k = (j + 13) / 27; k <= 10; i = 3 * (++k) - 1);
+    }
+}
---
+++ Exercise2.java
@@ -0,0 +1,21 @@
+public class Exercise2 {
+    public static void main(String[] args) {
+        int j, k;
+        switch(k){
+            case 1: case2:
+            j = 2 * k -1;
+            break;
+            case 3: case 5:
+            j = 3 * k + 1;
+            break;
+            case 4:j = 4 * k -1;
+            break;
+            case 6: case 7: case 8:
+            j = k -2;
+            break;
+            default:
+            System.out.printf("Error in switch, k =%d\n", k);  
+        }
+
+    } 
+}
